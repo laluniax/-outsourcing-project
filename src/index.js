069879e26d59e366563987ef1c7af9a438bcd692
hojun.yoon <tjdsksro@gmail.com>
2023-12-06 14:09:32 +0900

    react query 전역 설정

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import GlobalStyle from "GlobalStyle";
-import { Provider } from "react-redux";
-import getStore from "redux/config/configStore";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import GlobalStyle from 'GlobalStyle';
+import { Provider } from 'react-redux';
+import getStore from 'redux/config/configStore';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+const queryClient = new QueryClient();
 const store = getStore();
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-    <GlobalStyle />
-    <ToastContainer />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <Provider store={store}>
+      <App />
+      <GlobalStyle />
+      <ToastContainer />
+    </Provider>
+  </QueryClientProvider>
 );
